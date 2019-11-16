c09a4db565e90795c54a728d1b4f8c14f323029e
Naumov Konstantin <constantin.naumov@gmail.com>
2019-11-17 02:06:32 +0300

    Create UserType

---
+++ app/GraphQL/Types/UserType.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Types;
+
+use App\Models\User;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+/**
+ * Class UserType
+ * @package App\GraphQL\Types
+ */
+class UserType extends GraphQLType
+{
+    /**
+     * Attributes
+     * @var array
+     */
+    protected $attributes = [
+        'name' => 'User',
+        'description' => 'A user',
+        'model' => User::class,
+    ];
+
+    /**
+     * Get fields list
+     * @return array
+     */
+    public function fields(): array
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'The id of user'
+            ],
+            'email' => [
+                'type' => Type::string(),
+                'description' => 'The email of user'
+            ],
+            'password' => [
+                'type' => Type::string(),
+                'description' => 'The password of user'
+            ],
+        ];
+    }
+}
